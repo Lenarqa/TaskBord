7af471087191b984504a7d216184d622c38fd63c
Lenarqa <lenar.valiev211@gmail.com>
2020-02-06 23:32:55 +0700

    Now you can create new tasks and change it

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//document.querySelector('#add-new-part-of-card-btn').addEventListener('click', addNewCard);
-
 function addNewCard() {
     let newCard = document.createElement('div');
     newCard.className = "list-header";
@@ -7,10 +5,9 @@ function addNewCard() {
     let newCardText = document.getElementById('newCardText').value;
     //let newCardText = $('#newCardText').data('placeholder');
     
-    let cardText  = document.createElement('p');
-    cardText.className = 'p-change-class';
-    cardText.onclick = function(){changePtoTextArea()};
-    
+    let cardText = document.createElement('p');
+    //cardText.className = 'p-change-class';
+    cardText.onclick = function() {ChangeToTextArea(this);}
     
     if(newCardText == ""){
         alert("Вы забыли ввести текст задания!");
@@ -30,40 +27,14 @@ function addNewCard() {
     }
 }
 
-/*
-$(document).ready(function(){
-    $('.p-change-class').on('click', function(){
-        console.log('its work!');
-        let pastText = $(this).text();
-        $(this).replaceWith("<textarea id='textareaToP' cols='20' rows='10' onclick='changeTextAreaToP()'>" + pastText + "</textarea>");
-    });
-});
-*/
 
-function changePtoTextArea(){
-    let pastText = $('.p-change-class').text();
-    $('.p-change-class').replaceWith("<textarea id='textareaToP' cols='20' rows='10' onclick='changeTextAreaToP()'>" + pastText + "</textarea>");
-    //$('.textareaToP').replaceWith("<p class='p-change-class'>" + pastText + "</p>");
-    pastText = "";
+function ChangeToTextArea(element){
+    let pastText = element.innerHTML;
+    console.log(pastText);
+    $(element).replaceWith("<textarea  cols='10' rows='10' onblur='ChangeToP(this)'>" + pastText + "</textarea>");
 }
 
-function changeTextAreaToP(){
-        let pastText = $('#textareaToP').text();
-        $('#textareaToP').replaceWith("<p class='p-change-class' onclick='changePtoTextArea()'>" + pastText + "</p>");
-        pastText = "";
+function ChangeToP(element){
+    $(element).replaceWith("<p onclick='ChangeToTextArea(this)'>" + $(element).val() + "</p>");
+    let text = element.innerHTML;
 }
-
-//$('.list-content').on('click', '#textareaToP', changeTextAreaToP());
-
-
-
-
-/*
-function inputText(o){
-    return o.value;
-}
-*/
-//jquery
-//селекторы 'tag' '.класс' '#id'
-//
-//$('.add-new-part-of-card , header').css({'border':'1px solid yellow'});
